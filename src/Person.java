a97c6bd6f71da1d7266314396a4db3c0d643637a
lucybrooks <lucybrooks@users.noreply.github.com>
2014-12-11 08:13:39 -0700

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,1 +1,41 @@
-// Super Class to represent a person
+public class Person
+{
+  private String firstn;
+  private String lastn;
+  private int age;
+  
+  //Constructor
+  public Person(String newf, String newl, int newa)
+  {
+    firstn=newf;
+    lastn=newl;
+    age=newa;
+  }
+  
+  //Accessors
+  public String getFirstName()
+  {
+    return firstn;
+  }
+  
+  public String getLastName()
+  {
+    return lastn;
+  }
+  
+  public int getAge()
+  {
+    return age;
+  }
+  
+  public String toString()
+  {
+    return firstn+" , "+lastn;
+  } 
+  
+  //Mutators
+  public void recordBirthday()
+  {
+    age++;
+  }
+}
