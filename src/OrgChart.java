5852017e3a2f07a8677d834deef5e13a60a55922
lucybrooks <lucybrooks@users.noreply.github.com>
2014-12-11 08:13:02 -0700

    Update OrgChart.java

--- src/OrgChart.java
+++ src/OrgChart.java
@@ -1,1 +1,18 @@
-// main program to drive org chart
+public class OrgChart
+{
+  public static void main (String[]args)
+  {
+    Manager first=new Manager("Caeser", "Flickerman", 34, 44.7, 7, "Developement Manager", "Developement");
+    Employee pt=new Employee("Peeta", "Mellark", 18, 36.6, 7, "SR Developer");
+    Employee lt=new Employee("Katniss", "Everdeen", 18, 28.5, 7, "JR Developer");
+    Manager second=new Manager("Heavensbee", "Plutarch", 34, 44.7, 7, "Developement Manager", "Developement");
+    Employee pt2=new Employee("Finnick", "Odair", 24, 28.5, 7, "JR Developer");
+                                
+                                
+    System.out.println(first);
+    System.out.println("\t"+pt);
+    System.out.println("\t"+lt);
+    System.out.println(second);
+    System.out.println("\t"+pt2);  
+  }                             
+}
